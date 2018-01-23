898024518ffd88778a9ff19ee163fb2944c9040d
Paul Sztajer <paul@seethroughstudios.com>
2018-01-23 17:17:45 +0800

    Tween Settings for DOTween

---
+++ TweenSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+//namespace DG.Tweening
+//{
+    [System.Serializable]
+    public class TweenSettings
+    {
+        public float duration = 1;
+        public float delay = 0;
+        public Ease ease = Ease.Linear;
+        public int loops = 1;
+    }
+
+    public static class TweenUtils {
+        public static Tweener Set(this Tweener tweener, TweenSettings settings) {
+            return tweener.SetDelay(settings.delay)
+                          .SetEase(settings.ease)
+                          .SetLoops(settings.loops);
+        }
+    }
+//}
